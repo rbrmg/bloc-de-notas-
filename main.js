54db9cbd74704f685b0e49e6aeff14c106005fc2
CGG <elccorreodecristina13@gmail.com>
2023-12-14 17:51:18 +0100

    Endpoints y collecion Postman

--- main.js
+++ main.js
@@ -1,20 +1,100 @@
 //cuidado con el import de express (creo que esta inoperativo)
 import express from "express";
 import dotenv from "dotenv";
-import fileUpload from "express-fileupload";
 import cors from "cors";
-// import errors from "./src/controllers/controladorDeUsuarios"
 
 dotenv.config();
 
+// voy a leer el puerto en .env
+const { PORT } = process.env;
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static(process.env.UPLOADS_DIR));
-app.use(fileUpload());
-app.use(routes);
-app.use(errorController);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000: http://localhost:8080");
+app.post("/register", (req, res) => {
+  res.status(201).send({
+    status: "ok",
+    message: "Usuario creado",
+  });
+});
+
+app.post("/login", (req, res) => {
+  res.status(201).send({
+    status: "ok",
+    message: "Login",
+    data: {
+      token: "dfsgdrfgsdf",
+    },
+  });
+});
+
+app.get("/categories", (req, res) => {
+  res.status(200).send({
+    status: "ok",
+    message: "Listado categorias",
+    data: {
+      categories: [
+        { id: 1, name: "deporte" },
+        { id: 2, name: "personal" },
+      ],
+    },
+  });
+});
+
+app.get("/notes/:id", (req, res) => {
+  res.status(200).send({
+    status: "ok",
+    message: "Detalle nota",
+    data: {
+      id: req.params.id,
+    },
+  });
+});
+
+app.post("/notes", (req, res) => {
+  res.status(201).send({
+    status: "ok",
+    message: "Nota creada",
+    data: {
+      id: "1",
+      title: req.body.title,
+      text: req.body.text,
+      categoriaId: req.body.categoriaId,
+    },
+  });
+});
+
+app.put("/notes", (req, res) => {
+  res.status(200).send({
+    status: "ok",
+    message: "Nota modificada",
+    data: {
+      id: "1",
+      title: req.body.title,
+      text: req.body.text,
+      categoriaId: req.body.categoriaId,
+    },
+  });
+});
+
+app.get("/notes", (req, res)=>{
+  console.log(req.query)
+  res.status(200).send({
+    status:"ok",
+    message:"Listado notas",
+    data: []
+  })})
+
+app.use((req, res) => {
+  res.status(404).send({
+    status: "error",
+    message: "NOT FOUND",
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3000: http://localhost:" + PORT);
 });
